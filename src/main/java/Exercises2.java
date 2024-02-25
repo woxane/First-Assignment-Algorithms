7d5adfeb987bdfa95732604a589e8b1b42e0fad4
woxane <mohsen.minavand1384@gmail.com>
2024-02-25 21:47:30 +0330

    Implement twoSum function

--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -4,17 +4,17 @@ import java.util.List;
 import java.util.Map;
 
 public class Exercises2 {
-
-    /*
-    Given an array of integers nums and an integer target, return indices of the two numbers
-    such that they add up to target.
-
-    You may assume that each input would have exactly one solution, and you may not use the same element twice.
-    You can return the answer in any order.
-    */
-
     public int[] twoSum(int[] nums, int target) {
-        // TODO
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+
         return null;
     }
 
@@ -64,6 +64,12 @@ public class Exercises2 {
     }
 
     public static void main(String[] args) {
-        // test your code here!
+        Exercises2 exercises = new Exercises2();
+
+        // Test twoSum
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = exercises.twoSum(nums, target);
+        System.out.println("Two Sum: [" + result[0] + ", " + result[1] + "]");
     }
 }
