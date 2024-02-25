2b2d50aa07694b84ff2480476d1f6cf1a5edfffb
woxane <mohsen.minavand1384@gmail.com>
2024-02-25 21:54:06 +0330

    Implement permute and backtrack functions

--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -18,35 +18,6 @@ public class Exercises2 {
         return null;
     }
 
-    /*
-    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-
-    Symbol       Value
-    I             1
-    V             5
-    X             10
-    L             50
-    C             100
-    D             500
-    M             1000
-
-    For example, 2 is written as II in Roman numeral, just two ones added together.
-    12 is written as XII, which is simply X + II.
-    The number 27 is written as XXVII, which is XX + V + II.
-
-    Roman numerals are usually written largest to smallest from left to right.
-    However, the numeral for four is not IIII.
-    Instead, the number four is written as IV.
-    Because the one is before the five we subtract it making four.
-    The same principle applies to the number nine, which is written as IX.
-    There are six instances where subtraction is used:
-
-    I can be placed before V (5) and X (10) to make 4 and 9.
-    X can be placed before L (50) and C (100) to make 40 and 90.
-    C can be placed before D (500) and M (1000) to make 400 and 900.
-
-    Given a roman numeral, convert it to an integer.
-    */
 
     public int romanToInt(String s) {
         Map<Character, Integer> romanToInteger = new HashMap<>();
@@ -74,15 +45,28 @@ public class Exercises2 {
         return result;
     }
 
-    /*
-    Given an array nums of distinct integers, return all the possible permutations.
-    You can return the answer in any order.
-    */
 
     public List<List<Integer>> permute(int[] nums) {
-        // TODO
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums);
+        return result;
     }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int num : nums) {
+            if (!current.contains(num)) {
+                current.add(num);
+                backtrack(result, current, nums);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         Exercises2 exercises = new Exercises2();
@@ -96,5 +80,10 @@ public class Exercises2 {
         String romanNumeral = "XXVII";
         int intValue = exercises.romanToInt(romanNumeral);
         System.out.println("Roman to Integer: " + intValue);
+        // Test permute
+        int[] numsForPermute = {1, 2, 3};
+        List<List<Integer>> permutations = exercises.permute(numsForPermute);
+        System.out.println("Permutations: " + permutations);
+
     }
 }
